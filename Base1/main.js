c44131a8c4f4ed8bef3b2d27f900dc64f5a808da
Patricia Campos <patricia@krint.io>
2019-07-06 09:47:43 +1000

    Step 4

--- Base1/main.js
+++ Base1/main.js
@@ -12,3 +12,12 @@ alert(`Nom: ${name}\nPrenom: ${surname}\nVille: ${city}`);
 //Step 3: String concatenation
 let userName = prompt("What is your name?");
 alert(`Hello ${userName}`);
+
+//Step 4:
+const formData = () => {
+  const name = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const city = document.getElementById("city").value;
+  alert(`Hello, ${name} ${surname}, you are from ${city} `);
+  event.preventDefault();
+};
