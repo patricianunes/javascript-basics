eb3fed2759802a8ad4f20ceff3ac21e9eac34d71
Patricia Campos <patricia@krint.io>
2019-07-05 16:39:33 +1000

    Step 2

--- Base1/main.js
+++ Base1/main.js
@@ -1,3 +1,10 @@
 console.log("linda!");
+
+//Step 1: Variables
 const name = "Gabriela";
 alert(name);
+
+//Step 2
+const surname = "Campos";
+const city = "Sydney";
+alert(`Nom: ${name}\nPrenom: ${surname}\nVille: ${city}`);
